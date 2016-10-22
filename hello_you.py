f2e4a35c80e6b1ca527decba9f336ec1ba9662f9
naveen babu <naveen479@outlook.com>
2016-10-22 11:26:03 -0400

    hello

---
+++ hello_you.py
@@ -0,0 +1,3 @@
+#!/usr/bin/python
+person = raw_input("Enter your name: ");
+print "Hello", person
---
+++ openfile.py
@@ -0,0 +1,5 @@
+#!/usr/bin/python
+fo =open("test.txt","wb")
+print "name of the file:", fo.name
+fo.close()
+
